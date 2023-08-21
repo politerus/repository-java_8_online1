51d66be2136b71505e6c44a428b2be7dbe10fc78
roman <romkagm1992@gmail.com>
2023-08-21 22:48:26 +0300

    FinalBaseOperation

---
+++ h_w2_Base_Operation/src/main/java/ua/com/alevel/Decision.java
@@ -0,0 +1,55 @@
+package ua.com.alevel;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Decision {
+    public static String getNumber(String line) {
+        int sumNumbers = 0;
+        for (int i = 0; i < line.length(); i++) {
+            //проверяю, является ли символ с индексом i в строке line цифрой
+            if (Character.isDigit(line.charAt(i))) {
+                sumNumbers += Character.getNumericValue(line.charAt(i));
+            }//if
+        }//for
+        return String.valueOf(sumNumbers);
+    }//public static String getNumber
+
+    public static void getLetters(String line) {
+        Map<Character, Integer> charCountMap = new TreeMap<>();
+       //  проверяю, является ли текущий символ  буквой.
+        for (char letters : line.toCharArray()) {
+            //если буква в верхнем реестре преобразую ее в нижний реестр
+            //с помощью  toLowerCase
+            if (Character.isLetter(letters)) {
+                letters = Character.toLowerCase(letters);
+                //отслеживаю количество каждого символа в строке letters
+                charCountMap.put(letters, charCountMap.getOrDefault(letters, 0) + 1);
+            }
+        }
+        //счетчик
+        int counter = 1;
+        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
+            System.out.printf(counter + "," + " " + entry.getKey() + " - " + entry.getValue() + " ");
+            counter++;
+        }
+    }
+
+    // public static void getTime(Integer lessons) {
+       /* //уроки с маленьким перерывом
+        int shortBreakLessons = (lessons) / 2;
+        //уроки с большим перерывом
+        int longBreakLessons = (lessons - 1) / 2;
+        int totalMinutes = (lessons * 45 )+ (shortBreakLessons * 5 )+ (longBreakLessons * 15 + 9 * 60);
+        int inHours = totalMinutes / 60;
+        int inMinutes = totalMinutes % 60;
+        System.out.println("Время окончания урока: " +lessons +" в Часах"+ inHours + " В минутах " + inMinutes);
+
+        */
+
+
+
+        // Выводим время окончания урока
+        //  System.out.println("Время окончания урока: " + hours + " " + minutes);
+    //  }
+}
---
+++ h_w2_Base_Operation/src/main/java/ua/com/alevel/Main.java
@@ -0,0 +1,55 @@
+package ua.com.alevel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        //1. реализуйте задачу,
+        // которая принимает строку с консоли и вычленяет все
+        // числа и находит их сумму
+        //Пример:
+        //Входные данные: 1w4tt!7 Выходные данные: 12
+        System.out.println("Введите входные данные для первого задания");
+        String numbers = bufferedReader.readLine();
+
+        System.out.println(ua.com.alevel.Decision.getNumber(numbers));
+        //2. реализуйте задачу, которая принимает строку с консоли
+        // и вычленяет все символы латиницы/кириллицы и сортирует их,
+        // указывая количество вхождений каждого символа
+        //Пример:
+        //Входные данные: 1w4tt!7мс
+        // Выходные данные: 1. t - 2 2. w - 1
+
+        System.out.println("Введите входные данные для второго задания");
+        String letters = bufferedReader.readLine();
+
+        ua.com.alevel.Decision.getLetters(letters);
+        System.out.println();
+//3. Конец уроков
+//В некоторой школе занятия начинаются в 9:00.
+// Продолжительность урока — 45 минут, после 1-го, 3-го, 5-го и т.д.
+// уроков перемена 5 минут, а после 2-го, 4-го, 6-го и т.д. — 15 минут.
+//Определите, когда заканчивается указанный урок.
+//Данные вводятся с консоли
+//Входные данные: Дан номер урока (число от 1 до 10).
+//Выходные данные: Выведите два целых числа: время окончания урока в часах и минутах.
+//При решении этой задачи нельзя пользоваться цыклами и условными операторами инструкциями!!!
+//Примеры:
+//Входные данные: 3
+//Выходные данные:11 35
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите номер урока: ");
+        int lessonNumber = scanner.nextInt();
+        // Рассчитываю общую продолжительность уроков и перемен до окончания указанного урока
+        int totalDuration = (lessonNumber ) * 45 + (lessonNumber / 2) * 20;
+        // Рассчитываю часы и минуты окончания урока
+        int hours = 9 + totalDuration / 60;
+        int minutes = totalDuration % 60;
+        // Вывожу время окончания урока
+        System.out.println("Время окончания урока: " + hours + ":" + minutes);
+    }
+}
