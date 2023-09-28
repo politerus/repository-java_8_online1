240e0687c2f9fea15d95a3fc89a1234e1ef6cc81
roman <romkagm1992@gmail.com>
2023-09-28 20:06:15 +0300

    revork

--- h_w6_dictionary/src/main/java/ua/com/alevel/Dictionary.java
+++ h_w6_dictionary/src/main/java/ua/com/alevel/Dictionary.java
@@ -1,68 +1,73 @@
 package ua.com.alevel;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.Collection;
+import java.util.*;
 
 public class Dictionary<K, V> {
-    private final Map<K, V> map;
+    private static final int INITIAL_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private final Hashtable<K, V> hashtable;
 
     public Dictionary() {
-        map = new HashMap<>();
+        hashtable = new Hashtable<>(INITIAL_CAPACITY, LOAD_FACTOR);
     }
 
     public int size() {
-        return map.size();
+        return hashtable.size();
     }
 
     public boolean isEmpty() {
-        return map.isEmpty();
+        return hashtable.isEmpty();
     }
 
     public boolean containsKey(K key) {
-        return map.containsKey(key);
+        return hashtable.containsKey(key);
     }
 
     public boolean containsValue(V value) {
-        return map.containsValue(value);
+        return hashtable.containsValue(value);
     }
 
     public V get(K key) {
-        return map.get(key);
+        return hashtable.get(key);
     }
 
-    public void put(K key, V value) {
-        if (key == null || value == null) {
-            throw new IllegalArgumentException("Key and value cannot be null");
+    public boolean put(K key, V value) {
+        V oldValue = hashtable.put(key, value);
+        return !Objects.equals(oldValue, value);
+    }
+
+    public boolean remove(K key) {
+        return hashtable.remove(key) != null;
+    }
+
+    public boolean putAll(Dictionary<K, V> dictionary) {
+        boolean modified = false;
+        for (Map.Entry<K, V> entry : dictionary.entrySet()) {
+            if (put(entry.getKey(), entry.getValue())) {
+                modified = true;
+            }
         }
-        map.put(key, value);
+        return modified;
     }
 
-    public void remove(K key) {
-        map.remove(key);
-    }
-
-    public void putAll(Dictionary<K, V> dictionary) {
-        if (dictionary == null) {
-            throw new IllegalArgumentException("Dictionary cannot be null");
-        }
-
-        for (K key : dictionary.keySet()) {
-            V value = dictionary.get(key);
-            this.put(key, value);
-        }
-
-    }
-
-    public void clear() {
-        map.clear();
+    public boolean clear() {
+        hashtable.clear();
+        return true;
     }
 
     public Set<K> keySet() {
-        return map.keySet();
+        return hashtable.keySet();
     }
 
     public Collection<V> values() {
-        return map.values();
+        return hashtable.values();
+    }
+
+    public Set<Map.Entry<K, V>> entrySet() {
+        return hashtable.entrySet();
+    }
+
+    @Override
+    public String toString() {
+        return hashtable.toString();
     }
 }
--- h_w6_dictionary/src/main/java/ua/com/alevel/Main.java
+++ h_w6_dictionary/src/main/java/ua/com/alevel/Main.java
@@ -1,42 +1,45 @@
 package ua.com.alevel;
 
 import java.util.Collection;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) {
-       Dictionary<String, Integer> dictionary = new Dictionary<>();
+ public static void main(String[] args) {
+  Dictionary<String, Integer> dictionary = new Dictionary<>();
 
-        dictionary.put("one", 1);
-        dictionary.put("two", 2);
-        dictionary.put("three", 3);
+  dictionary.put("one", 1);
+  dictionary.put("two", 2);
+  dictionary.put("three", 3);
 
-        Collection<Integer> allValues = dictionary.values();
-        System.out.println("All values:");
-        for (Integer value : allValues) {
-            System.out.println(value);
-        }
 
-        System.out.println("Size: " + dictionary.size());
+  Set<String> keys = dictionary.keySet();
+  Collection<Integer> values = dictionary.values();
 
-        System.out.println("Is empty: " + dictionary.isEmpty());
+  System.out.println("Keys: " + keys);
+  System.out.println("Values: " + values);
 
-        System.out.println("Contains key 'two': " + dictionary.containsKey("two"));
+  System.out.println("Size: " + dictionary.size());
 
-        System.out.println("Contains value 4: " + dictionary.containsValue(4));
+  System.out.println("Is empty: " + dictionary.isEmpty());
 
-        System.out.println("Value for key 'one': " + dictionary.get("one"));
+  System.out.println("Contains key 'two': " + dictionary.containsKey("two"));
+  System.out.println("Contains value 4: " + dictionary.containsValue(4));
 
-        dictionary.remove("three");
-        System.out.println("After removing 'three', size: " + dictionary.size());
+  System.out.println("Value for key 'two': " + dictionary.get("two"));
 
-        Dictionary<String, Integer> anotherDictionary = new Dictionary<>();
-        anotherDictionary.put("four", 4);
-        anotherDictionary.put("five", 5);
+  Dictionary<String, Integer> anotherDictionary = new Dictionary<>();
+  anotherDictionary.put("four", 4);
+  anotherDictionary.put("five", 5);
+  dictionary.putAll(anotherDictionary);
 
-        dictionary.putAll(anotherDictionary);
-        System.out.println("After putting all elements from another dictionary, size: " + dictionary.size());
+  System.out.println("Updated Dictionary: " + dictionary);
 
-        dictionary.clear();
-        System.out.println("After clearing, size: " + dictionary.size());
-    }
+  dictionary.remove("three");
+
+  System.out.println("Size after removing 'three': " + dictionary.size());
+
+  dictionary.clear();
+
+  System.out.println("Size after clearing: " + dictionary.size());
+ }
 }
