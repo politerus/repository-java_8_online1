cd3f4b2e7fa53be8bf4ac1df719e72d8d9d2102b
roman <romkagm1992@gmail.com>
2023-10-03 21:15:08 +0300

    8

--- h_w6_dictionary/src/main/java/ua/com/alevel/Dictionary.java
+++ h_w6_dictionary/src/main/java/ua/com/alevel/Dictionary.java
@@ -1,73 +1,193 @@
 package ua.com.alevel;
+
 import java.util.*;
 
 public class Dictionary<K, V> {
-    private static final int INITIAL_CAPACITY = 16;
+
+
+    private static final int DEFAULT_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
-    private final Hashtable<K, V> hashtable;
+
+    private Entry<K, V>[] table;
+    private int size;
 
     public Dictionary() {
-        hashtable = new Hashtable<>(INITIAL_CAPACITY, LOAD_FACTOR);
+        this.table = new Entry[DEFAULT_CAPACITY];
+        this.size = 0;
     }
 
     public int size() {
-        return hashtable.size();
+        return size;
     }
 
     public boolean isEmpty() {
-        return hashtable.isEmpty();
+        return size == 0;
     }
 
     public boolean containsKey(K key) {
-        return hashtable.containsKey(key);
+        return getEntry(key) != null;
     }
 
     public boolean containsValue(V value) {
-        return hashtable.containsValue(value);
+        for (Entry<K, V> entry : table) {
+            while (entry != null) {
+                if (Objects.equals(entry.getValue(), value)) {
+                    return true;
+                }
+                entry = entry.next;
+            }
+        }
+        return false;
     }
 
     public V get(K key) {
-        return hashtable.get(key);
+        Entry<K, V> entry = getEntry(key);
+        return (entry != null) ? entry.getValue() : null;
     }
 
     public boolean put(K key, V value) {
-        V oldValue = hashtable.put(key, value);
-        return !Objects.equals(oldValue, value);
+        if (key == null) {
+            throw new NullPointerException("Key cannot be null");
+        }
+        int index = hash(key) % table.length;
+        Entry<K, V> entry = table[index];
+        while (entry != null) {
+            if (key.equals(entry.getKey())) {
+                entry.setValue(value);
+                return true;
+            }
+            entry = entry.next;
+        }
+        addEntry(index, key, value);
+        size++;
+        if (size >= table.length * LOAD_FACTOR) {
+            resizeTable();
+        }
+        return true;
     }
 
     public boolean remove(K key) {
-        return hashtable.remove(key) != null;
+        if (key == null) {
+            return false;
+        }
+        int index = hash(key) % table.length;
+        Entry<K, V> entry = table[index];
+        Entry<K, V> prev = null;
+        while (entry != null) {
+            if (key.equals(entry.getKey())) {
+                if (prev != null) {
+                    prev.next = entry.next;
+                } else {
+                    table[index] = entry.next;
+                }
+                size--;
+                return true;
+            }
+            prev = entry;
+            entry = entry.next;
+        }
+        return false;
     }
 
     public boolean putAll(Dictionary<K, V> dictionary) {
-        boolean modified = false;
-        for (Map.Entry<K, V> entry : dictionary.entrySet()) {
-            if (put(entry.getKey(), entry.getValue())) {
-                modified = true;
+        if (dictionary != null) {
+            for (Entry<K, V> entry : dictionary.table) {
+                while (entry != null) {
+                    put(entry.getKey(), entry.getValue());
+                    entry = entry.next;
+                }
             }
+            return true;
         }
-        return modified;
+        return false;
     }
 
     public boolean clear() {
-        hashtable.clear();
+        Arrays.fill(table, null);
+        size = 0;
         return true;
     }
 
     public Set<K> keySet() {
-        return hashtable.keySet();
+        Set<K> keys = new HashSet<>();
+        for (Entry<K, V> entry : table) {
+            while (entry != null) {
+                keys.add(entry.getKey());
+                entry = entry.next;
+            }
+        }
+        return keys;
     }
 
     public Collection<V> values() {
-        return hashtable.values();
+        List<V> values = new ArrayList<>();
+        for (Entry<K, V> entry : table) {
+            while (entry != null) {
+                values.add(entry.getValue());
+                entry = entry.next;
+            }
+        }
+        return values;
     }
 
-    public Set<Map.Entry<K, V>> entrySet() {
-        return hashtable.entrySet();
+    private int hash(K key) {
+        return (key == null) ? 0 : key.hashCode();
     }
 
-    @Override
-    public String toString() {
-        return hashtable.toString();
+    private Entry<K, V> getEntry(K key) {
+        int index = hash(key) % table.length;
+        Entry<K, V> entry = table[index];
+        while (entry != null) {
+            if (key.equals(entry.getKey())) {
+                return entry;
+            }
+            entry = entry.next;
+        }
+        return null;
+    }
+
+    private void addEntry(int index, K key, V value) {
+        Entry<K, V> entry = new Entry<>(key, value);
+        entry.next = table[index];
+        table[index] = entry;
+    }
+
+    private void resizeTable() {
+        int newCapacity = table.length * 2;
+        Entry<K, V>[] newTable = new Entry[newCapacity];
+        for (Entry<K, V> entry : table) {
+            while (entry != null) {
+                int index = hash(entry.getKey()) % newCapacity;
+                Entry<K, V> next = entry.next;
+                entry.next = newTable[index];
+                newTable[index] = entry;
+                entry = next;
+            }
+        }
+        table = newTable;
+    }
+
+    private static class Entry<K, V> {
+        private final K key;
+        private V value;
+        private Entry<K, V> next;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
     }
 }
--- h_w6_dictionary/src/main/java/ua/com/alevel/Main.java
+++ h_w6_dictionary/src/main/java/ua/com/alevel/Main.java
@@ -1,45 +1,52 @@
 package ua.com.alevel;
 
-import java.util.Collection;
-import java.util.Set;
+
 
 public class Main {
  public static void main(String[] args) {
+  // Create a Dictionary
   Dictionary<String, Integer> dictionary = new Dictionary<>();
 
+  // Add key-value pairs
   dictionary.put("one", 1);
   dictionary.put("two", 2);
   dictionary.put("three", 3);
 
-
-  Set<String> keys = dictionary.keySet();
-  Collection<Integer> values = dictionary.values();
-
-  System.out.println("Keys: " + keys);
-  System.out.println("Values: " + values);
-
+  // Check size and isEmpty
   System.out.println("Size: " + dictionary.size());
+  System.out.println("Is Empty: " + dictionary.isEmpty());
 
-  System.out.println("Is empty: " + dictionary.isEmpty());
+  // Check containsKey and containsValue
+  System.out.println("Contains Key 'two': " + dictionary.containsKey("two"));
+  System.out.println("Contains Value 3: " + dictionary.containsValue(3));
 
-  System.out.println("Contains key 'two': " + dictionary.containsKey("two"));
-  System.out.println("Contains value 4: " + dictionary.containsValue(4));
+  // Get a value by key
+  System.out.println("Value for Key 'one': " + dictionary.get("one"));
 
-  System.out.println("Value for key 'two': " + dictionary.get("two"));
+  // Remove a key-value pair
+  dictionary.remove("two");
+  System.out.println("After removing 'two', Size: " + dictionary.size());
 
+  // Create another Dictionary
   Dictionary<String, Integer> anotherDictionary = new Dictionary<>();
   anotherDictionary.put("four", 4);
   anotherDictionary.put("five", 5);
+
+  // Put all entries from another Dictionary
   dictionary.putAll(anotherDictionary);
+  System.out.println("After putting all entries from another Dictionary, Size: " + dictionary.size());
 
-  System.out.println("Updated Dictionary: " + dictionary);
-
-  dictionary.remove("three");
-
-  System.out.println("Size after removing 'three': " + dictionary.size());
-
+  // Clear the Dictionary
   dictionary.clear();
+  System.out.println("After clearing, Size: " + dictionary.size());
 
-  System.out.println("Size after clearing: " + dictionary.size());
+  // Create a new Dictionary
+  Dictionary<String, Integer> newDictionary = new Dictionary<>();
+  newDictionary.put("six", 6);
+  newDictionary.put("seven", 7);
+
+  // Get keySet and values
+  System.out.println("Keys: " + newDictionary.keySet());
+  System.out.println("Values: " + newDictionary.values());
  }
 }
